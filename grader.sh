1d5ad882cf5150ce798a4179d9fca118908357ac
Diana Doan <cs15lsp23ft@ieng6-201.ucsd.edu>
2023-06-08 13:37:39 -0700

    changed grader.sh

--- grader.sh
+++ grader.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 javac -cp $CPATH *.java
 java -cp $CPATH org.junit.runner.JUnitCore LocationerTests 
